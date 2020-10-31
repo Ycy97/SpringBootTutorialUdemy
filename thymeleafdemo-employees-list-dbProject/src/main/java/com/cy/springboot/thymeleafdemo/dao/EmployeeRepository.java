832814c7a0b56be546ecbc70355e34f79d421730
ChengYee <chengyeeyap97@gmail.com>
2020-10-31 22:15:33 +0800

    SpringBoot Thymeleaf with DB real-time Project + CRUD operations

---
+++ thymeleafdemo-employees-list-dbProject/src/main/java/com/cy/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.cy.springboot.thymeleafdemo.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cy.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	//that's it... no need to write implementation class
+	
+	//add a method to sort by lastName
+	public List<Employee> findAllByOrderByLastNameAsc(); 
+}
